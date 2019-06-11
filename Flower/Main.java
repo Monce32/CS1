eeabd5bf62dc05b8934a770f85aa936ae711a5de
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-11 12:37:45 -0600

    Create Main.java

---
+++ Flower/Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[]args) {
+		    Flower flower1 = new Flower(1, 3, "Rose");
+		    Flower flower2 = new Flower(2, 5, "Sunflower");
+				
+		    System.out.println("Flower 1 name = " + flower1.nameOfFlower);
+		    System.out.println("Flower 2 name = " + flower2.nameOfFlower);
+		
+    }
+}

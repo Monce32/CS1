a15d412ac01bbe1869476cf95805742ea76b1a92
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-11 12:41:31 -0600

    Update Flower.java

--- Flower/Flower.java
+++ Flower/Flower.java
@@ -1,13 +1,11 @@
 class Flower {
-	int color;
-	int typeOfFlower;
-	String nameOfFlower;
+    int color;
+    int typeOfFlower;
+    String nameOfFlower;
 	
-	public Flower(int c, int t, String nf) {
-	   this.color = c;
-		 this.typeOfFlower = t;
-		 this.nameOfFlower = nf;
-		
-	}
-	
+    public Flower(int c, int t, String nf) {
+        this.color = c;
+	this.typeOfFlower = t;
+	this.nameOfFlower = nf;
+    }
 }

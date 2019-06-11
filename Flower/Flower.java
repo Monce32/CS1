aaeac3879ae65ca60f1beecb56cc32b34a6fa407
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-11 12:35:27 -0600

    Create Flower.java

---
+++ Flower/Flower.java
@@ -0,0 +1,13 @@
+class Flower {
+	int color;
+	int typeOfFlower;
+	String nameOfFlower;
+	
+	public Flower(int c, int t, String nf) {
+	   this.color = c;
+		 this.typeOfFlower = t;
+		 this.nameOfFlower = nf;
+		
+	}
+	
+}

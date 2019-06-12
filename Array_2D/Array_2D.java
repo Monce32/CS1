1c57d9436da3d656373904e8a6176a80c4c24297
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-12 11:40:28 -0600

    Create Array_2D.java

---
+++ Array_2D/Array_2D.java
@@ -0,0 +1,24 @@
+public class Array_2D {
+
+  public static void main(String [] args) {
+		int count = 0;
+		int [][] nums = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+		int row;
+		int col;
+		
+		for(int top = 0; top < nums.length; top++) {
+			for(row = top; row >= 0; row--) {
+				col = top - row;
+				nums[row][col] = count;
+				count++;
+			}
+		}
+		
+		for(row = 0; row < nums.length; row++) {
+			for(col = 0; col < nums[row].length; col++) {
+				System.out.print(nums[row][col] + " ");
+			}
+			System.out.println();
+		}
+	}
+}

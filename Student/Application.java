20017abb810cd9a3bab9319fb1bc9669c6d9b425
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-12 12:33:16 -0600

    Update Application.java

--- Student/Application.java
+++ Student/Application.java
@@ -2,30 +2,30 @@ import java.util.*;
 
 public class Application {
 
-	public static void main(String [] args) {
-		Scanner scnr = new Scanner(System.in);
-		ArrayList <Student> studentList = new ArrayList <Student> ();
-		Application obj = new Application();
+    public static void main(String [] args) {
+        Scanner scnr = new Scanner(System.in);
+	ArrayList <Student> studentList = new ArrayList <Student> ();
+	Application obj = new Application();
 		
-		obj.fillStudentArray(studentList, scnr);
-	}
+	obj.fillStudentArray(studentList, scnr);
+    }
 	
-	public void fillStudentArray(ArrayList <Student> studentList, Scanner x) {
-		int stGrade;
-		String stName = "";
+    public void fillStudentArray(ArrayList <Student> studentList, Scanner x) {
+        int stGrade;
+	String stName = "";
 		
-		for(int i = 0; i < 5; i++) {
-			System.out.println("Enter the name of the student");
-			stName = x.nextLine();
-			System.out.println("Enter the grade of the student");
-			stGrade = x.nextInt();
-			x.next();
+	for(int i = 0; i < 5; i++) {
+	    System.out.println("Enter the name of the student");
+	    stName = x.nextLine();
+	    System.out.println("Enter the grade of the student");
+	    stGrade = x.nextInt();
+	    x.next();
 			
-			studentList.add(new Student(stName, stGrade));
-		}
-		
-		for(int a = 0; a < studentList.size(); a++) {
-			System.out.println(studentList.get(a));
-		}
+	    studentList.add(new Student(stName, stGrade));
 	}
+		
+	for(int a = 0; a < studentList.size(); a++) {
+	    System.out.println(studentList.get(a));
+	}
+    }
 }

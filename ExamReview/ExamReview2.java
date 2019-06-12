919c90e5e063d111bd049c21eff75e0bd5417518
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-12 12:11:19 -0600

    Update ExamReview2.java

--- ExamReview/ExamReview2.java
+++ ExamReview/ExamReview2.java
@@ -3,33 +3,33 @@ import java.io.*;
 
 public class ExamReview2 {
 
-	public static void main(String [] args)throws IOException {
+    public static void main(String [] args)throws IOException {
 		
-		ExamReview2 obj = new ExamReview2();
+        ExamReview2 obj = new ExamReview2();
 		
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter file name: " );
-		String inputTextFile = input.next();
-		ArrayList <Integer> fileList = new ArrayList <Integer>();
-		System.out.println("Number of elements = " + obj.readArrayList(inputTextFile, fileList));
-		input.close();
-	}
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter file name: " );
+	String inputTextFile = input.next();
+	ArrayList <Integer> fileList = new ArrayList <Integer>();
+	System.out.println("Number of elements = " + obj.readArrayList(inputTextFile, fileList));
+	input.close();
+    }
 	
-	public int readArrayList(String fileName, ArrayList <Integer> fileList)throws IOException {
+    public int readArrayList(String fileName, ArrayList <Integer> fileList)throws IOException {
 		
-		Scanner inFile = new Scanner (new File(fileName));
-		while(inFile.hasNextLine()) {
+        Scanner inFile = new Scanner (new File(fileName));
+	    while(inFile.hasNextLine()) {
 			
-			String line = inFile.nextLine();
-			StringTokenizer inputTokens = new StringTokenizer(line, ",");
+	        String line = inFile.nextLine();
+		StringTokenizer inputTokens = new StringTokenizer(line, ",");
 			
-			while(inputTokens.hasMoreTokens()) {
-				fileList.add(Integer.parseInt(inputTokens.nextToken()));
-				//intList[index] = inputTokens.nextInteger();
+		while(inputTokens.hasMoreTokens()) {
+		    fileList.add(Integer.parseInt(inputTokens.nextToken()));
+		    //intList[index] = inputTokens.nextInteger();
 			
-			}
 		}
-		inFile.close();
-		return fileList.size();
-	}
+	    }
+	    inFile.close();
+	    return fileList.size();
+    }
 }

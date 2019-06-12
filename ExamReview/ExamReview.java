cd0d07445a1d7155ddab46143557eb10e1743c98
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-12 12:01:25 -0600

    Create ExamReview.java

---
+++ ExamReview/ExamReview.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class ExamReview {
+
+	public static void main(String [] args)throws IOException {
+		
+		ExamReview obj = new ExamReview();
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter file name: " );
+		String inputTextFile = input.next();
+		int [] inputArray = new int[20];
+		int x = obj.readArray(inputTextFile, inputArray);
+		System.out.println("Number of elements = " + x);
+		input.close();
+	}
+	
+	public int readArray(String fileName, int[] intList)throws IOException {
+		int index = 0;
+		
+		Scanner inFile = new Scanner (new File(fileName));
+		while(inFile.hasNextLine()) {
+			
+			String line = inFile.nextLine();
+			StringTokenizer inputTokens = new StringTokenizer(line, ",");
+			
+			while(inputTokens.hasMoreTokens()) {
+				intList[index] = Integer.parseInt(inputTokens.nextToken());
+				//intList[index] = inputTokens.nextInteger();
+				index++;
+			}
+		}
+		inFile.close();
+		return index;
+	}
+}

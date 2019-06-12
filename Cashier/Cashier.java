319f0b61a3bdc82f4e4a4876038bea3b74bf9536
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-12 10:42:12 -0600

    Update Cashier.java

--- Cashier/Cashier.java
+++ Cashier/Cashier.java
@@ -1,9 +1,9 @@
 /*
- * Name:		 Moncerrat Romero
- * Purpose: 	 Uses the basic fundamentals of a normal cashier
- * 				 by asking the user to input the item and calculates the cost
+ * Name: Moncerrat Romero
+ * Purpose: Uses the basic fundamentals of a normal cashier
+ * 	    by asking the user to input the item and calculates the cost
  * Date created: 02/05/2018
- * Class:		 CS 1050
+ * Class: CS 1050
 */
 
 import java.util.Scanner;
@@ -11,31 +11,31 @@ import java.util.Scanner;
 class Cashier {
 Scanner scnr = new Scanner(System.in);
 	
-	public static void main(String [] args) {
-		//calls an empty constructor
-		Cashier csh = new Cashier();
-		csh.acceptInput();
+    public static void main(String [] args) {
+        //calls an empty constructor
+	Cashier csh = new Cashier();
+	csh.acceptInput();
 	
-}
+    }
 
-	public void acceptInput(    ) {
-		String item;
-		double price;
-		int quantity;
+    public void acceptInput(    ) {
+        String item;
+	double price;
+	int quantity;
 		
-		System.out.println("Enter the item name");
-		item = scnr.nextLine();
-		System.out.println("Enter the price of the item");
-		price = scnr.nextDouble();
-		System.out.println("Enter the quantity");
-		quantity = scnr.nextInt();
+	System.out.println("Enter the item name");
+	item = scnr.nextLine();
+	System.out.println("Enter the price of the item");
+	price = scnr.nextDouble();
+	System.out.println("Enter the quantity");
+	quantity = scnr.nextInt();
 		
 		double tPrice = calcTotalPrice(quantity, price);
 		System.out.println("Total price is: " + tPrice);
-	}
+    }
 	
-	public double calcTotalPrice(int q, double price) {
+    public double calcTotalPrice(int q, double price) {
 		
-		return q * price;
-	}
+	return q * price;
+    }
 }

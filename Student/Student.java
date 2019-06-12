ff1126bf288117d72d888991c130f5bdc9d6ee72
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-12 12:18:36 -0600

    Create Student.java

---
+++ Student/Student.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Student {
+
+	private String name;
+	private int grade;
+	
+	public Student() {
+		
+	}
+	
+	public Student(String name, int grade) {
+		this.name = name;
+		this.grade = grade;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getGrade() {
+		return grade;
+	}
+	
+	public void setName(String x) {
+		name = x;
+	}
+	
+	public void setGrade(int y) {
+		grade = y;
+	}
+	
+	@Override
+	
+	public String toString() {
+		return "Name " + name + 
+				" Grade " + grade;
+	}
+}

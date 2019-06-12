631a5711103e5c9cdc8cfbb71ea335454c4242cb
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-12 11:45:25 -0600

    Create Array2D.java

---
+++ Array2D/Array2D.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Array2D {
+
+	public static void main(String [] args) {
+		int [] [] tempReadings = new int [3][3];
+		Array2D obj = new Array2D();
+		Scanner scnr = new Scanner(System.in);
+		obj.inputTempReadings(tempReadings, scnr);
+		
+	}
+	
+	public void inputTempReadings(int[][] tempInputs, Scanner x) {
+		
+		for(int row = 0; row < tempInputs.length; row++) {
+			for(int col = 0; col < tempInputs[row].length; col++) {
+				System.out.println("Enter the temp reading: ");
+				tempInputs[row][col] = x.nextInt();
+			}
+		}
+		printArray(tempInputs);
+	}
+	
+	public void printArray(int [][] temps) {
+		for(int row = 0; row < temps.length; row++) {
+			for(int col = 0; col < temps[row].length; col++) {
+				System.out.print(temps[row][col] + " ");
+			}
+			System.out.println();
+		}
+	}
+}

250d078dd3d5aeab9afe5daff34d8acbe69f2e37
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-12 12:05:06 -0600

    Update ExamReview.java

--- ExamReview/ExamReview.java
+++ ExamReview/ExamReview.java
@@ -3,35 +3,35 @@ import java.io.*;
 
 public class ExamReview {
 
-	public static void main(String [] args)throws IOException {
+    public static void main(String [] args)throws IOException {
 		
-		ExamReview obj = new ExamReview();
+        ExamReview obj = new ExamReview();
 		
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter file name: " );
-		String inputTextFile = input.next();
-		int [] inputArray = new int[20];
-		int x = obj.readArray(inputTextFile, inputArray);
-		System.out.println("Number of elements = " + x);
-		input.close();
-	}
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter file name: " );
+	String inputTextFile = input.next();
+	int [] inputArray = new int[20];
+	int x = obj.readArray(inputTextFile, inputArray);
+	System.out.println("Number of elements = " + x);
+	input.close();
+    }
 	
-	public int readArray(String fileName, int[] intList)throws IOException {
-		int index = 0;
-		
-		Scanner inFile = new Scanner (new File(fileName));
-		while(inFile.hasNextLine()) {
+    public int readArray(String fileName, int[] intList)throws IOException {
+        int index = 0;	
+	Scanner inFile = new Scanner (new File(fileName));
+	    
+	while(inFile.hasNextLine()) {
 			
-			String line = inFile.nextLine();
-			StringTokenizer inputTokens = new StringTokenizer(line, ",");
+	    String line = inFile.nextLine();
+	    StringTokenizer inputTokens = new StringTokenizer(line, ",");
 			
-			while(inputTokens.hasMoreTokens()) {
-				intList[index] = Integer.parseInt(inputTokens.nextToken());
-				//intList[index] = inputTokens.nextInteger();
-				index++;
-			}
-		}
-		inFile.close();
-		return index;
+	    while(inputTokens.hasMoreTokens()) {
+	        intList[index] = Integer.parseInt(inputTokens.nextToken());
+		//intList[index] = inputTokens.nextInteger();
+		index++;
+	    }
 	}
+	inFile.close();
+	return index;
+    }
 }

2f2f9d75a6ada9284482ea61f21b1b565b4f22cb
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-13 10:30:30 -0600

    Update CompareText.java

--- CompareText/CompareText.java
+++ CompareText/CompareText.java
@@ -7,47 +7,47 @@
 import java.util.Scanner;
 
 public class CompareText {
-	public static void main(String [] args) {
-		Scanner scnr = new Scanner(System.in);
-		CompareText obj = new CompareText();
-		obj.acceptInput(scnr);
-	}
+    public static void main(String [] args) {
+	Scanner scnr = new Scanner(System.in);
+	CompareText obj = new CompareText();
+	obj.acceptInput(scnr);
+    }
 	
-	public void acceptInput(Scanner scnr) {
-		String polygonType = "";
-		double side, length, width;
+    public void acceptInput(Scanner scnr) {
+	String polygonType = "";
+	double side, length, width;
 		
-		System.out.println("Enter a polygon");
-		polygonType = scnr.next();
+	System.out.println("Enter a polygon");
+	polygonType = scnr.next();
 		
-		if(polygonType.equalsIgnoreCase("square")) {
-			System.out.println("You selected square");
-			System.out.println("Enter the side length");
-			side = scnr.nextDouble();
-			System.out.println("\nThe area is: " + Square(side));
-		}
-		
-		else if(polygonType.equalsIgnoreCase("rectangle")) {
-			System.out.println("You selected rectangle");
-			System.out.println("Enter the length");
-			length = scnr.nextDouble();
-			System.out.println("Enter the width");
-			width = scnr.nextDouble();
-			System.out.println("\nThe area is: " + Rectangle(width, length));
-		}
-		
-		else {
-			System.out.println("Invalid shape");
-		}
+	if(polygonType.equalsIgnoreCase("square")) {
+	    System.out.println("You selected square");
+	    System.out.println("Enter the side length");
+	    side = scnr.nextDouble();
+	    System.out.println("\nThe area is: " + Square(side));
 	}
+		
+	else if(polygonType.equalsIgnoreCase("rectangle")) {
+	    System.out.println("You selected rectangle");
+	    System.out.println("Enter the length");
+	    length = scnr.nextDouble();
+	    System.out.println("Enter the width");
+	    width = scnr.nextDouble();
+	    System.out.println("\nThe area is: " + Rectangle(width, length));
+	}
+		
+	else {
+	    System.out.println("Invalid shape");
+	}
+    }
 	
-	public double Rectangle(double width, double length) {
+    public double Rectangle(double width, double length) {
 		
-		return width * length;
-	}
+        return width * length;
+    }
 	
-	public double Square(double side) {
+    public double Square(double side) {
 		
-		return side * side;
-	}
+	return side * side;
+    }
 }

97f156bb2c1243dc9506fd6c890da0d8b00d24d2
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-13 11:00:36 -0600

    Update StudentGrades.java

--- StudentGrades/StudentGrades.java
+++ StudentGrades/StudentGrades.java
@@ -2,33 +2,34 @@ import java.util.Scanner;
 
 public class StudentGrades {
 	
-	Scanner scnr = new Scanner(System.in);
+    Scanner scnr = new Scanner(System.in);
 	
-	public static void main(String [] args) {
+    public static void main(String [] args) {
 		
-		int [] studentGrades = new int [5];
-		StudentGrades obj = new StudentGrades();
-		obj.inputGrades(studentGrades);
-	}
+    int [] studentGrades = new int [5];
+    StudentGrades obj = new StudentGrades();
+    obj.inputGrades(studentGrades);
+    }
 	
-	public void inputGrades(int [] list) {
+    public void inputGrades(int [] list) {
 		
-		for(int i = 0; i < 5; i++) {
-			System.out.print("\nEnter grade. " + (i + 1));
-			list[i] =  scnr.nextInt();
-		}
-		
-		for(int a = 0; a < list.length; a++) {
-			System.out.println(list[a]);
-		}
-		
-		for(int b = 0; b < list.length; b++) {
-			if(list[b] >= 50) {
-				System.out.println("Pass");
-			}
-			else {
-				System.out.println("Fail");
-			}
-		}
+	for(int i = 0; i < 5; i++) {
+	    System.out.print("\nEnter grade. " + (i + 1));
+	    list[i] =  scnr.nextInt();
 	}
+		
+	for(int a = 0; a < list.length; a++) {
+	    System.out.println(list[a]);
+	}
+		
+	for(int b = 0; b < list.length; b++) {
+	    if(list[b] >= 50) {
+	    System.out.println("Pass");
+	    }
+		
+	    else {
+	    System.out.println("Fail");
+	    }
+        }
+    }
 }

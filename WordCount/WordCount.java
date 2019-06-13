9c793a990745b1b887f15afc4e689bccc86813ff
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-13 11:05:12 -0600

    Update WordCount.java

--- WordCount/WordCount.java
+++ WordCount/WordCount.java
@@ -2,30 +2,28 @@ import java.util.Scanner;
 
 public class WordCount {
 	
-	public static void main(String [] args) {
-		Scanner scnr = new Scanner(System.in);
-		WordCount obj = new WordCount();
-		System.out.println("Enter a sentence");
-		String sentence = scnr.nextLine();
-		System.out.println("Number of words " + obj.countWords(sentence));
-	}
+    public static void main(String [] args) {
+        Scanner scnr = new Scanner(System.in);
+	WordCount obj = new WordCount();
+	System.out.println("Enter a sentence");
+	String sentence = scnr.nextLine();
+	System.out.println("Number of words " + obj.countWords(sentence));
+    }
 
-	public int countWords(String sentence) {
-		int numWords = 0;
-		int counter = 0;
-		char space = ' ';
+    public int countWords(String sentence) {
+	int numWords = 0;
+	int counter = 0;
+	char space = ' ';
 		
-		if (sentence.length() > 0) {
-			numWords = 1;
-			while(counter <= sentence.length() - 1) {
-				if(sentence.charAt(counter) == space) {
+	if (sentence.length() > 0) {
+	    numWords = 1;
+	    while(counter <= sentence.length() - 1) {
+	        if(sentence.charAt(counter) == space) {
 				
-				}
-			counter ++;
-			}
-		
 		}
-		return numWords;
-	}
-	
+	        counter ++;
+	    }
+        }
+	return numWords;
+    }	
 }

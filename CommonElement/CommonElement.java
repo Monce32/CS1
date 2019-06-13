f468478f91452dd17797a2aeb8134f598549021d
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-13 11:40:14 -0600

    Update CommonElement.java

--- CommonElement/CommonElement.java
+++ CommonElement/CommonElement.java
@@ -2,23 +2,23 @@ import java.util.*;
 
 public class CommonElement {
 
-	public static void main(String [] args) {
-		int[] my_array1 = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
-		int[] my_array2 = {25, 14, 56, 8, 7, 6, 77, 18, 29, 49};
-		int[] commonArray = new int [my_array1.length];
-		commonArray = findCommon(my_array1, my_array2);
-		System.out.print(Arrays.toString(commonArray));
-	}
+    public static void main(String [] args) {
+	int[] my_array1 = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+	int[] my_array2 = {25, 14, 56, 8, 7, 6, 77, 18, 29, 49};
+	int[] commonArray = new int [my_array1.length];
+	commonArray = findCommon(my_array1, my_array2);
+	System.out.print(Arrays.toString(commonArray));
+    }
 	
-	public static int [] findCommon(int [] array1, int [] array2) {
-		int [] commonElements = new int [array1.length];
-		for(int i = 0; i < array1.length; i++) {
-			for(int j = 0; j < array2.length; j++) {
-				if(array1[i] == array2[j]) {
-					commonElements [i] = array1[i];
-				}
-			}
+    public static int [] findCommon(int [] array1, int [] array2) {
+	int [] commonElements = new int [array1.length];
+	    for(int i = 0; i < array1.length; i++) {
+		for(int j = 0; j < array2.length; j++) {
+		    if(array1[i] == array2[j]) {
+			commonElements [i] = array1[i];
+		    }
 		}
-		return commonElements;
-	}
+	    }
+	    return commonElements;
+    }
 }

2ebe3f2ba5f32cddaeab85f84472e3fd2efe383a
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-13 11:31:29 -0600

    Update AvgScores.java

--- AvgScores/AvgScores.java
+++ AvgScores/AvgScores.java
@@ -7,34 +7,33 @@ import java.util.Scanner;
 
 public class AvgScores {
 
-	public static void main(String [] args) {
-		Scanner scnr = new Scanner(System.in);
-		AvgScores obj = new AvgScores();
-		obj.acceptScores(scnr);
-	}
+    public static void main(String [] args) {
+	Scanner scnr = new Scanner(System.in);
+	AvgScores obj = new AvgScores();
+	obj.acceptScores(scnr);
+    }
 	
-	public void acceptScores(Scanner x) {
-		double curScores;
-		double maxScore = 0;
-		double minScore = 100;
+    public void acceptScores(Scanner x) {
+	double curScores;
+	double maxScore = 0;
+	double minScore = 100;
 		
-		System.out.println("Enter five scores.");
+	System.out.println("Enter five scores.");
 		
-		for(int numScores = 1; numScores <= 5; numScores++) {
-			curScores = x.nextDouble();
-			System.out.println("Your score is: " + curScores);
+	for(int numScores = 1; numScores <= 5; numScores++) {
+	    curScores = x.nextDouble();
+	    System.out.println("Your score is: " + curScores);
 		
-			if(curScores > maxScore) {
-				maxScore = curScores;
-			}
+	    if(curScores > maxScore) {
+		maxScore = curScores;
+	    }
 			
-			else if(curScores < minScore) {
-				minScore = curScores;
-			}
+	    else if(curScores < minScore) {
+		minScore = curScores;
+	    }
 			
-		}
-		
-		System.out.println("Max score is " + maxScore);
-		System.out.println("Min score is " + minScore);
-	}
+        }
+        System.out.println("Max score is " + maxScore);
+	System.out.println("Min score is " + minScore);
+    }
 }

bce19636b9738301bc2526bfde62c69023f895b9
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-13 11:07:58 -0600

    Create Rectangle.java

---
+++ Rectangle/Rectangle.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		acceptInput();
+	}
+	
+	public static void acceptInput(){
+		double width;
+		double length;
+		
+		Scanner scnr = new Scanner(System.in);
+		
+		System.out.println("Enter width: ");
+		width = scnr.nextInt();
+		
+		System.out.println("Enter length: ");
+		length = scnr.nextInt();
+		
+		System.out.println("The area of the rectangle is: " + 
+							calcArea(width, length));
+		System.out.println("The perimeter of the rectangle is: " + 
+							calcPerimeter(width, length));
+	}
+	
+	public static double calcArea(double x, double y ){
+		
+		double areaOfRect = x * y;
+		
+		return areaOfRect;
+	}
+	
+	public static double calcPerimeter(double x, double y) {
+		
+		double periOfRect = (x + x) + (y + y);
+		
+		return periOfRect;
+	}
+}

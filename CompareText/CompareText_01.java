956f0fec91d3a891bd7e9b060d30fa23c4e27055
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-13 10:36:00 -0600

    Update CompareText_01.java

--- CompareText/CompareText_01.java
+++ CompareText/CompareText_01.java
@@ -2,62 +2,63 @@ import java.util.Scanner;
 
 public class CompareText_01 {
 	
-	public static void main(String [] args) {
-			Scanner scnr = new Scanner(System.in);
-			CompareText_01 obj = new CompareText_01();
-			obj.acceptInput(scnr);
-		}
+    public static void main(String [] args) {
+	Scanner scnr = new Scanner(System.in);
+	CompareText_01 obj = new CompareText_01();
+	obj.acceptInput(scnr);
+    }
 		
-	public void acceptInput(Scanner scnr) {
-			String polygonType = "";
+    public void acceptInput(Scanner scnr) {
+	String polygonType = "";
 			
-			System.out.println("Enter a polygon");
-			polygonType = scnr.next();
+	System.out.println("Enter a polygon");
+	polygonType = scnr.next();
 			
-			switch(polygonType) {
+	switch(polygonType) {
 			
-			case "Square":
-			case "square":	
-				displaySquareDetails(scnr);
-				break;
+	case "Square":
+	case "square":	
+	    displaySquareDetails(scnr);
+	    break;
 				
-			case "Rectangle":
-			case "rectangle":	
-				displayRectangleDetails(scnr);
-				break;
+	case "Rectangle":
+	case "rectangle":	
+	    displayRectangleDetails(scnr);
+	    break;
 				
-			default:
-				System.out.println("Invalid shape");
-				break;
+	default:
+	    System.out.println("Invalid shape");
+	    break;
 		
-			}
-		}
-		public void displaySquareDetails(Scanner scnr) {
-			double side;
-			System.out.println("You selected square");
-			System.out.println("Enter the side length");
-			side = scnr.nextDouble();
-			System.out.println("\nThe area is: " + Square(side));
-			
-		}
-		
-		public void displayRectangleDetails(Scanner scnr) {
-			double length, width;
-			System.out.println("You selected rectangle");
-			System.out.println("Enter the length");
-			length = scnr.nextDouble();
-			System.out.println("Enter the width");
-			width = scnr.nextDouble();
-			System.out.println("\nThe area is: " + Rectangle(width, length));
-		}
-		
-		public double Rectangle(double width, double length) {
-			
-			return width * length;
-		}
-		
-		public double Square(double side) {
-			
-			return side * side;
-		}
 	}
+    }
+	
+    public void displaySquareDetails(Scanner scnr) {
+        double side;
+	System.out.println("You selected square");
+	System.out.println("Enter the side length");
+	side = scnr.nextDouble();
+        System.out.println("\nThe area is: " + Square(side));
+			
+    }
+		
+    public void displayRectangleDetails(Scanner scnr) {
+	double length, width;
+	System.out.println("You selected rectangle");
+	System.out.println("Enter the length");
+	length = scnr.nextDouble();
+	System.out.println("Enter the width");
+	width = scnr.nextDouble();
+	System.out.println("\nThe area is: " + Rectangle(width, length));
+    }
+		
+    public double Rectangle(double width, double length) {
+			
+        return width * length;
+    }
+		
+    public double Square(double side) {
+			
+        return side * side;
+    }
+}

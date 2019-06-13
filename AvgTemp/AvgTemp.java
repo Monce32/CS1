89420040d6fe1b22281302a5093575d1f19554e8
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-13 11:33:00 -0600

    Create AvgTemp.java

---
+++ AvgTemp/AvgTemp.java
@@ -0,0 +1,36 @@
+/*
+ * Accept daily temperature readings for five week days
+ * and display the Max temperature which is greater than
+ * the weekly average temperature.
+ */
+
+import java.util.Scanner;
+
+public class AvgTemp {
+
+	public static void main(String [] args) {
+		Scanner scnr = new Scanner(System.in);
+		AvgTemp obj = new AvgTemp();
+		obj.acceptReadings(scnr);
+	}
+	
+	public void acceptReadings(Scanner x) {
+		double totalTemp = 0;
+		double temp, avg;
+		double maxTemp = 0;
+		
+		for(int dayNum = 1; dayNum <= 5; dayNum++) {
+			System.out.println("Enter temperature " + dayNum);
+			temp = x.nextDouble();
+			totalTemp = totalTemp + temp;
+			avg = totalTemp / dayNum;
+			System.out.println("Average temperature: " + avg);
+			
+			if(temp > avg) {
+				maxTemp = temp;
+			}
+		}
+		
+		System.out.println("Weekly max temperature is: " + maxTemp);
+	}
+}

c9ee27466d51a0ca0530019a248fe3fd6a56f5c8
Moncerrat (or Monce) Romero <42752076+Monce32@users.noreply.github.com>
2019-06-13 11:09:44 -0600

    Update Rectangle.java

--- Rectangle/Rectangle.java
+++ Rectangle/Rectangle.java
@@ -2,41 +2,41 @@ import java.util.Scanner;
 
 public class Rectangle {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 		
-		acceptInput();
-	}
+	acceptInput();
+    }
 	
-	public static void acceptInput(){
-		double width;
-		double length;
+    public static void acceptInput(){
+	double width;
+	double length;
 		
-		Scanner scnr = new Scanner(System.in);
+	Scanner scnr = new Scanner(System.in);
 		
-		System.out.println("Enter width: ");
-		width = scnr.nextInt();
+	System.out.println("Enter width: ");
+	width = scnr.nextInt();
 		
-		System.out.println("Enter length: ");
-		length = scnr.nextInt();
+	System.out.println("Enter length: ");
+	length = scnr.nextInt();
 		
-		System.out.println("The area of the rectangle is: " + 
+	System.out.println("The area of the rectangle is: " + 
 							calcArea(width, length));
-		System.out.println("The perimeter of the rectangle is: " + 
+	System.out.println("The perimeter of the rectangle is: " + 
 							calcPerimeter(width, length));
-	}
+    }
 	
-	public static double calcArea(double x, double y ){
+    public static double calcArea(double x, double y ){
 		
-		double areaOfRect = x * y;
+	double areaOfRect = x * y;
 		
-		return areaOfRect;
-	}
+	return areaOfRect;
+    }
 	
-	public static double calcPerimeter(double x, double y) {
+    public static double calcPerimeter(double x, double y) {
 		
-		double periOfRect = (x + x) + (y + y);
+	double periOfRect = (x + x) + (y + y);
 		
-		return periOfRect;
-	}
+	return periOfRect;
+    }
 }
